0674197987f19aac70d5e7090e2edce7b51de2fd
ericschn <eschneider327@gmail.com>
2018-12-04 16:46:24 -0600

    justin's solution and fixed mine

--- day1/1-2.js
+++ day1/1-2.js
@@ -10,37 +10,43 @@ numInput.split("\n").forEach(a => {
 });
 
 // Test data
-intArr = [1, -1]; // 0
-intArr = [3, 3, 4, -2, -4]; // 10
-//intArr = [-6, 3, 8, 5, -6]; // 5
-//intArr = [7, 7, -2, -7, -4]; // 14
+// intArr = [1, -1]; // 0
+// intArr = [3, 3, 4, -2, -4]; // 10
+// intArr = [-6, 3, 8, 5, -6]; // 5
+intArr = [7, 7, -2, -7, -4]; // 14
 
 //console.log(sumArr);
-
+var counter = 1;
 while(!duplicateFound) {
+  //console.log("sumArr at top of while: " + sumArr);
+  console.log(counter);
   addToSumArray();
+  if (counter > 2) break;
+  counter++;
 }
 
-console.log(intArr);
-console.log(sumArr);
+//console.log(intArr);
+//console.log(sumArr);
 console.log(duplicate);
 
 //console.log(sumArr);
 
 // Build sumArr
 // Probably should be a pure function?
-function incrementSumArray(sumArrInput, intArrInput) {
-  let intArrLen = intArrInput.length;
-  // grab the last index of previos sumArr - can maybe use pop() and put it in sumsEndArr?
-  let prevSumArrEnd = sumArrInput[sumArrInput.length-1];
-  // run through intArr and push sums to sumArr
-  for (let i = 0; i < intArrLen; i++) {
-  }
-}
+// function incrementSumArray(sumArrInput, intArrInput) {
+//   let intArrLen = intArrInput.length;
+//   // grab the last index of previos sumArr - can maybe use pop() and put it in sumsEndArr?
+//   let prevSumArrEnd = sumArrInput[sumArrInput.length-1];
+//   // run through intArr and push sums to sumArr
+//   for (let i = 0; i < intArrLen; i++) {
+//   }
+// }
 
 // impure function version :)
 function addToSumArray() {
-  let sumArrIndex = sumArr[sumArr.length-1];
+  let sumArrIndex = sumArr.length-1;
+  //console.log("sumArr length: " + sumArr.length);
+  //console.log("sumArrIndex is: " + sumArrIndex);
   for (let i = 0; i < intArr.length; i++) {
     sumArr.push(sumArr[sumArrIndex + i] + intArr[i]);
 
@@ -54,8 +60,9 @@ function addToSumArray() {
 }
 
 function checkDupes() {
-  for (let i = 0; i < sumArr.length; i++) {
-    for (let j = i+1; j < sumArr.length; j++) {
+  let sumArrLen = sumArr.length;
+  for (let i = 0; i < sumArrLen; i++) {
+    for (let j = i+1; j < sumArrLen; j++) {
       if (sumArr[i] == sumArr[j]) {
         //console.log("duplicate found: " + sumArr[i]);
         return sumArr[i];
