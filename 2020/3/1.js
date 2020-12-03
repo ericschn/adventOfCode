2118c12172d87ccd1fd5120d963c0f20028027ea
Eric Schneider <eschneider327@gmail.com>
2020-12-03 13:57:57 -0600

    renaming variables for clarity

--- 2020/3/1.js
+++ 2020/3/1.js
@@ -2,15 +2,16 @@
 const fs = require('fs');
 let input = fs.readFileSync('input.txt', 'utf-8').split("\n");
 
-let counter = 0;
-let treeCount = 0;
+const slope = 3
 let lineLength = input[0].length;
+let xPos = 0;
+let treeCount = 0;
 
 for (line of input) {
-  if (line[counter % lineLength] == '#') {
+  if (line[xPos % lineLength] == '#') {
     treeCount++;
   }
-  counter += 3;
+  xPos += slope;
 }
 
 console.log(treeCount);
--- 2020/3/2.js
+++ 2020/3/2.js
@@ -3,22 +3,21 @@ const fs = require('fs');
 let input = fs.readFileSync('input.txt', 'utf-8').split("\n");
 
 const slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]];
-let counter = 0;
+let xPos = 0;
 let lineLength = input[0].length;
 let treeCount = 0;
-let treeCounts = [];
+let treeCountArray = [];
 
 for (slope of slopes) {
-  console.log(slope);
-  counter = 0;
+  xPos = 0;
   treeCount = 0;
   for (let i = 0; i < input.length; i += slope[1]) {
-      if (input[i][counter % lineLength] == '#') {
+      if (input[i][xPos % lineLength] == '#') {
         treeCount++;
       }
-    counter += slope[0]
+    xPos += slope[0]
   }
-  treeCounts.push(treeCount);
+  treeCountArray.push(treeCount);
 }
 
-console.log(treeCounts.reduce((a,b) => a * b, 1));
+console.log(treeCountArray.reduce((a,b) => a * b, 1));
