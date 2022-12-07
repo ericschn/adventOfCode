868bf5850acb8b3acf449e7e9de2e86c88faf505
ericschn <eschneider327@gmail.com>
2022-12-06 23:10:04 -0600

    simplify 2022 day 6

--- 2022/6/1.js
+++ 2022/6/1.js
@@ -3,17 +3,13 @@
 const fs = require('fs');
 let input = fs.readFileSync('input.txt', 'utf-8');
 
-for (let i = 0; i < input.length; i++) {
-  if (
-    input[i] !== input[i + 1] &&
-    input[i] !== input[i + 2] &&
-    input[i] !== input[i + 3] &&
-    input[i + 1] !== input[i + 2] &&
-    input[i + 1] !== input[i + 3] &&
-    input[i + 2] !== input[i + 3]
-  ) {
+for (let i = 0; i < input.length - 4; i++) {
+  let set = new Set();
+  for (let j = 0; j < 4; j++) {
+    set.add(input[i + j]);
+  }
+  if (set.size === 4) {
     console.log(i + 4);
-    console.log(input[i] + input[i + 1] + input[i + 2] + input[i + 3]);
     break;
   }
 }
