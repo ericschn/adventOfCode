8612ca1dcebd6c3d50b770df00e0de805385fbba
ericschn <eschneider327@gmail.com>
2023-12-02 12:42:27 -0600

    start day 2 part 2

--- 2023/02/2.swift
+++ 2023/02/2.swift
@@ -2,8 +2,13 @@ import Foundation
 
 let input = try String(contentsOfFile: "./input.txt").components(separatedBy: "\n")
 let reg = try Regex("([2-9]\\d|1[3-9])\\sr|([2-9]\\d|1[4-9])\\sg|([2-9]\\d|1[5-9])\\sb")
+var rgb = [0, 0, 0];
 var sum = 0;
 for (i, line) in input.enumerated() {
-  if !line.contains(reg) { sum += i + 1 }
+  // find largest red value in line
+  // then green
+  // then blue
+  // multiply them together
+  // add value to sum
 }
 print(sum)
