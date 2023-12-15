b1064abdad3ca0460c7475dd505d3ebc707a020f
ericschn <eschneider327@gmail.com>
2023-12-15 16:21:41 -0600

    day 5 part 2 done

--- 2023/05/2.swift
+++ 2023/05/2.swift
@@ -5,46 +5,28 @@ class Category {
   var translations: [CategoryTranslation] = []
 
   func translateRange(_ inputRange: Range<Int>) -> [Range<Int>] {
-    // Takes in one range and returns array of ranges
-    // after processing and translating if necessary
     var result: [Range<Int>] = []
     var currentRanges = [inputRange]
     var nextRanges: [Range<Int>] = []
 
-    // iterate over the translations
     for translation in translations {
       for currentRange in currentRanges {
-        // Check currentRange for overlap
         if currentRange.overlaps(translation.range) {
-          // If overlap, translate overlapping portion by offset and
-          // put into result array
           let lo = max(currentRange.lowerBound, translation.range.lowerBound)
           let hi = min(currentRange.upperBound, translation.range.upperBound)
           let overlap = lo+translation.offset..<hi+translation.offset
-          print("We incremented \(lo..<hi) by \(translation.offset) so it's now \(overlap)")
           result.append(overlap)
-          // put any overhanging ranges into nextRanges
           let before = currentRange.lowerBound..<lo
           if before.count > 0 { nextRanges.append(before) }
           let after = hi..<currentRange.upperBound
           if after.count > 0 { nextRanges.append(after) }
-          print("before: \(before) | after: \(after)")
-
         } else {
-          // currentRange does not overlap translation.range
           nextRanges.append(currentRange)
         }
-
-        // Put overhangs into nextRanges
       }
-      // moving to next translation, move nextRanges into currentRanges
       currentRanges = nextRanges
-      // empty out nextRanges
-      // print("before nextRanges.removeAll: \(currentRanges)")
       nextRanges.removeAll()
-      // print("after nextRanges.removeAll: \(currentRanges)")
     }
-    // add nextRanges to result
     result += currentRanges
     return result
   }
@@ -102,21 +84,15 @@ let input = try String(contentsOfFile: "./input.txt")
 let initialSeeds: [Range<Int>] = parseSeeds(seedsLine: input[0])
 let categories = parseCategories(data: input)
 
-// print(categories)
-
-// var finalResult: [Range<Int>] = []
 var resultArr: [Range<Int>] = initialSeeds
 var temp: [Range<Int>] = []
-for category in categories {
-  print("\(category.title) START")
 
+for category in categories {
   for seed in resultArr {
     temp += category.translateRange(seed)
   }
   resultArr = temp
   temp.removeAll()
-  print("resultArr = \(resultArr)")
-    print("\(category.title) DONE\n\n")
 }
 
 print((resultArr.sorted { $0.lowerBound < $1.lowerBound })[0].lowerBound)
